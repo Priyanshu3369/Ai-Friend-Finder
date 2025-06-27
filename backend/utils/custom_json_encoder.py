3e2d537afdde3688a00744e2af1fbcfa09c45b27
Priyanshu3369 <priyanshuchourasiya32198@gmail.com>
2025-06-28 00:21:57 +0530

    starting fresh

--- backend/utils/custom_json_encoder.py
+++
@@ -1,8 +0,0 @@
-from flask.json.provider import DefaultJSONProvider
-from bson import ObjectId
-
-class CustomJSONProvider(DefaultJSONProvider):
-    def default(self, obj):
-        if isinstance(obj, ObjectId):
-            return str(obj)
-        return super().default(obj)
