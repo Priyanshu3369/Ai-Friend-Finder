8cba4a10690d19b22f9becf1bbb9ddce825aa35a
Priyanshu3369 <priyanshuchourasiya32198@gmail.com>
2025-06-28 10:38:20 +0530

    done with the chats

--- backend/config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    SECRET_KEY = 'super-secret-key'
-    JWT_SECRET_KEY = 'jwt-secret-key'
-    MONGO_URI = 'mongodb+srv://priyanshuarcade07:cNcciKAGlCBJiKzw@cluster0.gmeynja.mongodb.net/friend?retryWrites=true&w=majority'
